e1f453b6f669128bef343dfc550e8a2b2edfc65a
mk2ran <mi2ran@naver.com>
2017-05-29 22:33:33 +0900

    2017-05-29 로그인 창!

---
+++ app/src/main/java/com/example/nt910s5k_k71g/thinketch/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.example.nt910s5k_k71g.thinketch;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        final Button button1 = (Button) findViewById(R.id.button1);
+        final Button button2 = (Button) findViewById(R.id.button2);
+        final Button button3 = (Button) findViewById(R.id.button3);
+        final Button button4 = (Button) findViewById(R.id.button4);
+        final Button button5 = (Button) findViewById(R.id.button5);
+        final Button button6 = (Button) findViewById(R.id.button6);
+        final Button button7 = (Button) findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent outIntent = new Intent(LoginActivity.this, MainActivity.class);
+                LoginActivity.this.startActivity(outIntent);
+            }
+        });
+
+
+    }
+}
--- app/src/main/java/com/example/nt910s5k_k71g/thinketch/MainActivity.java
+++ app/src/main/java/com/example/nt910s5k_k71g/thinketch/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.nt910s5k_k71g.thinketch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText idText = (EditText) findViewById(R.id.idText);
+        EditText pwText = (EditText) findViewById(R.id.pwText);
+
+
+        final Button logbutton = (Button) findViewById(R.id.logbutton);
+        final Button regbutton = (Button) findViewById(R.id.regbutton);
+
+        logbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(loginIntent);
+            }
+        });
+
     }
 }
